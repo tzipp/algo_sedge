c395ecf434fde47aa9b271920449cd4dc5605ce4
tfarrell-vz <farrell@abiresearch.com>
2016-11-02 15:04:24 -0400

    Test QF

--- union_find/test_union_find.py
+++ union_find/test_union_find.py
@@ -20,15 +20,21 @@ class TestQuickFind:
 
     def test_0_1_joined(self):
         self.qf.union(0, 1)
+        # Test connectedness by direct inspection.
         assert self.qf.ids[0] == 1
         assert self.qf.ids[1] == 1
         for i in range(2, 10):
             assert self.qf.ids[i] != 0 and self.qf.ids[i] != 1
 
+        # Test connectedness by the QF API
+        assert self.qf.connected(0,1)
+
     def test_0_1_then_7_9_union(self):
         self.qf.union(0, 1)
         self.qf.union(7, 9)
-        joined = set([0, 1, 7, 9])
+        joined = {0, 1, 7, 9}
+
+        # Test connectedness by directly comparing data.
         assert self.qf.ids[0] == 1
         assert self.qf.ids[1] == 1
         assert self.qf.ids[7] == 9
@@ -37,6 +43,10 @@ class TestQuickFind:
             if i not in joined:
                 assert self.qf.ids[i] not in joined
 
-    def test_0_1_then_7_9_then_0_9(self):
+        # Test using connected function
+        assert self.qf.connected(0, 1)
+        assert self.qf.connected(7, 9)
+
+    def test_0_1_then_7_9_then_0_9_union(self):
         pass
 
